60dc33f862f4cfc7ea4332b70080740dd9c44eca
Kevin DeAlmeida <dealmeida.dak@gmail.com>
2025-06-02 18:05:48 +0200

    update schema.sql

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,21 +1,83 @@
-create table user (
-  id int unsigned primary key auto_increment not null,
-  email varchar(255) not null unique,
-  password varchar(255) not null
+CREATE TABLE user (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  username VARCHAR(20) NOT NULL,  
+  mail VARCHAR(255) UNIQUE NOT NULL,
+  password VARCHAR(145) NOT NULL,
+  avatar VARCHAR(255) NOT NULL,
+  city VARCHAR(60) NOT NULL,
+  has_pet BOOLEAN NOT NULL,
+  exposition ENUM("Nord","Sud","Est","Ouest") DEFAULT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
-create table item (
-  id int unsigned primary key auto_increment not null,
-  title varchar(255) not null,
-  user_id int unsigned not null,
-  foreign key(user_id) references user(id)
+CREATE TABLE plant (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+  icon VARCHAR(20) NOT NULL,
+  description TEXT NOT NULL,
+  plant_exposition VARCHAR(10) NOT NULL,
+  sowing VARCHAR(100) NOT NULL,
+  watering INT NOT NULL,
+  harvesting VARCHAR(100) NOT NULL,
+  toxic BOOLEAN,
+  edible BOOLEAN
 );
 
-insert into user(id, email, password)
-values
-  (1, "jdoe@mail.com", "123456");
+CREATE TABLE garden (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  user_id INT,
+  FOREIGN KEY (user_id) REFERENCES user(id)
+);
 
-insert into item(id, title, user_id)
-values
-  (1, "Stuff", 1),
-  (2, "Doodads", 1);
+CREATE TABLE user_plant_photo (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  picture VARCHAR(255) NOT NULL,
+  garden_id INT, 
+  FOREIGN KEY (garden_id) REFERENCES garden(id)
+);
+
+CREATE TABLE tag (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  tagname VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE tutorial (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  url VARCHAR(255) NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  description TEXT NOT NULL,
+  author VARCHAR(255) NOT NULL,
+  duration TIME
+);
+
+CREATE TABLE plant_garden (
+  garden_id INT,
+  plant_id INT,
+  PRIMARY KEY (garden_id, plant_id)
+);
+
+CREATE TABLE plant_tag (
+  plant_id INT,
+  tag_id INT,
+  PRIMARY KEY (plant_id, tag_id)
+);
+
+CREATE TABLE tag_tutorial (
+  tutorial_id INT,
+  tag_id INT,
+  PRIMARY KEY (tutorial_id, tag_id)
+);
+
+CREATE TABLE user_tutorial (
+  user_id INT,
+  tutorial_id INT,
+  PRIMARY KEY (user_id, tutorial_id)
+);
+
+
+
+
+
