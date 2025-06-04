232c02fe9602eebd4d54c6aed4b258aebc94624a
tommarghem <t.marghem@gmail.com>
2025-06-04 16:33:16 +0200

    First01

---
+++ client/src/components/Dashboard/Dashboard.tsx
@@ -0,0 +1,63 @@
+import "./dashboard.css";
+
+export default function Dashboard() {
+  return (
+    <div className="dashboard">
+      {/* Header */}
+      <div className="header">
+        <div className="user-info">
+          <img src="/user-avatar.png" alt="User" />
+          <span>User_420</span>
+        </div>
+        <div className="weather">
+          <div>24°C</div>
+          <div className="small-text">Partiellement nuageux</div>
+        </div>
+      </div>
+
+      <article>
+        <h2>
+          Journal de bord <span className="info-icon">ℹ️</span>
+        </h2>
+      </article>
+
+      {/* Journal de bord */}
+      <div className="journal">
+        <div className="plant-carousel">
+          <div className="plant-card">
+            <img src="/plants/aloe.png" alt="Aloe" />
+            <div>Aloe</div>
+          </div>
+          <div className="plant-card active">
+            <img src="/plants/ficus.png" alt="Ficus" />
+            <div>Ficus</div>
+          </div>
+          <div className="plant-card">
+            <img src="/plants/aloe.png" alt="Aloe" />
+            <div>Aloe</div>
+          </div>
+        </div>
+      </div>
+      <article>
+        <h2>Recommandé pour toi</h2>
+      </article>
+      {/* Recommandé */}
+      <div className="recommended">
+        <div className="recommended-images">
+          <img src="/recommended/1.jpg" alt="Recommandation 1" />
+          <img src="/recommended/2.jpg" alt="Recommandation 2" />
+        </div>
+      </div>
+
+      <article>
+        <h2>A propos des plantes </h2>
+      </article>
+
+      {/* Fait amusant */}
+      <div className="fact-box">
+        La plus grande fleur individuelle du monde est le rafflesia (Rafflesia
+        arnoldii). Elle peut atteindre 1 m de diamètre et peser jusqu’à 11 kg.
+      </div>
+    </div>
+  );
+}
