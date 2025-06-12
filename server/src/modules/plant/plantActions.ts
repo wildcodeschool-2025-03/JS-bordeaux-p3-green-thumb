d8ad836bb01444185839be06573bb2499d18690e
S1mon_G <simonguillo@gmail.com>
2025-06-12 16:06:38 +0200

    modified import order on plantActions #biomeismyfriend

--- server/src/modules/plant/plantActions.ts
+++ server/src/modules/plant/plantActions.ts
@@ -1,6 +1,6 @@
 import type { RequestHandler } from "express";
-import plantRepository from "./plantRepository";
 import { StatusCodes } from "http-status-codes";
+import plantRepository from "./plantRepository";
 
 const browse: RequestHandler = async (req, res) => {
   const plantDB = await plantRepository.readAll();
