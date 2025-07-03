a97dff111ceae8b04ae313deb87ac2af16989e86
S1mon_G <simonguillo@gmail.com>
2025-07-03 13:53:15 +0200

    modified css for register

--- client/src/pages/register/Register.tsx
+++ client/src/pages/register/Register.tsx
@@ -15,38 +15,45 @@ export default function Register() {
   };
 
   return (
-    <form onSubmit={submitRegister}>
-      <section className="user-infos">
-        <fieldset className="form-field">
-          <label htmlFor="firstname">firstname</label>
-          <input ref={firstnameRef} type="text" />
-        </fieldset>
-        <fieldset className="form-field">
-          <label htmlFor="lastname">lastname</label>
-          <input ref={lastnameRef} type="text" />
-        </fieldset>
-        <fieldset className="form-field">
-          <label htmlFor="username">username</label>
-          <input ref={usernameRef} type="text" />
-        </fieldset>
-        <fieldset className="form-field">
-          <label htmlFor="city">city</label>
-          <input ref={cityRef} type="text" />
-        </fieldset>
-        <fieldset className="form-field">
-          <label htmlFor="email">email</label>
-          <input ref={emailRef} type="text" />
-        </fieldset>
-        <fieldset className="form-field">
-          <label htmlFor="passwword">password</label>
-          <input ref={passwordRef} type="password" />
-        </fieldset>
-        <fieldset className="form-field">
-          <label htmlFor="confirm password">consfirm password</label>
-          <input ref={confirmPasswordRef} type="text" />
-        </fieldset>
-      </section>
-      <button type="submit">Send</button>
-    </form>
+    <main className="form-wrapper">
+      <h2>Register</h2>
+      <form onSubmit={submitRegister}>
+        <section className="user-infos">
+          <fieldset className="form-field">
+            <label htmlFor="firstname">firstname</label>
+            <input ref={firstnameRef} type="text" className="form-input" />
+          </fieldset>
+          <fieldset className="form-field">
+            <label htmlFor="lastname">lastname</label>
+            <input ref={lastnameRef} type="text" className="form-input" />
+          </fieldset>
+          <fieldset className="form-field">
+            <label htmlFor="username">username</label>
+            <input ref={usernameRef} type="text" className="form-input" />
+          </fieldset>
+          <fieldset className="form-field">
+            <label htmlFor="city">city</label>
+            <input ref={cityRef} type="text" className="form-input" />
+          </fieldset>
+          <fieldset className="form-field">
+            <label htmlFor="email">email</label>
+            <input ref={emailRef} type="text" className="form-input" />
+          </fieldset>
+          <fieldset className="form-field">
+            <label htmlFor="passwword">password</label>
+            <input ref={passwordRef} type="password" className="form-input" />
+          </fieldset>
+          <fieldset className="form-field">
+            <label htmlFor="confirm password">consfirm password</label>
+            <input
+              ref={confirmPasswordRef}
+              type="text"
+              className="form-input"
+            />
+          </fieldset>
+        </section>
+        <button type="submit">Register</button>
+      </form>
+    </main>
   );
 }
