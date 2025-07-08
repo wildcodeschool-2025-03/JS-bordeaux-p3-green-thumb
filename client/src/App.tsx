388c7a05f7b13140fc43e0d571e3319087645a2b
tommarghem <t.marghem@gmail.com>
2025-07-08 10:16:18 +0200

    fix router front

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,10 @@
 import { Outlet } from "react-router";
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
-import Landing from "./pages/landing/Landing";
 
 function App() {
   return (
     <>
-      <Landing />
       <Outlet />
       <Navbar />
     </>
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,33 +1,8 @@
 import { createRoot } from "react-dom/client";
-import { RouterProvider, createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router";
 import "./styles/_variables.css";
+import router from "./router";
 
-/* ************************************************************************* */
-
-// Import the main app component
-import App from "./App";
-
-// Import additional components for new routes
-// Try creating these components in the "pages" folder
-
-// import About from "./pages/About";
-// import Contact from "./pages/Contact";
-
-/* ************************************************************************* */
-
-// Create router configuration with routes
-// You can add more routes as you build out your app!
-const router = createBrowserRouter([
-  {
-    path: "/", // The root path
-    element: <App />, // Renders the App component for the home page
-  },
-  // Try adding a new route! For example, "/about" with an About component
-]);
-
-/* ************************************************************************* */
-
-// Find the root element in the HTML document
 const rootElement = document.getElementById("root");
 if (rootElement == null) {
   throw new Error(`Your HTML Document should contain a <div id="root"></div>`);
