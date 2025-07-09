d02b015f4d32c14c733ce30bb53d0256f46c50f8
S1mon_G <simonguillo@gmail.com>
2025-07-09 15:13:54 +0200

    added error messages to username and password field

--- client/src/pages/register/Register.tsx
+++ client/src/pages/register/Register.tsx
@@ -10,9 +10,10 @@ import "./Register.css";
 export default function Register() {
   const navigate = useNavigate();
 
-  const [isComplete, setIsComplete] = useState(false);
   const [type, setType] = useState("password");
   const [icon, setIcon] = useState(eyeOff);
+  const [isComplete, setIsComplete] = useState(false);
+  const [usernameError, setUsernameError] = useState("");
   const [passwordError, setPasswordError] = useState("");
 
   const firstnameRef = useRef<HTMLInputElement>(null);
@@ -29,13 +30,16 @@ export default function Register() {
   const confirmPasswordRef = useRef<HTMLInputElement>(null);
 
   const validateForm = () => {
+    const username = usernameRef.current?.value ?? "";
+    const usernameValid = username.length > 0 && username.length <= 20;
+
     const passwordMatch =
       passwordRef.current?.value === confirmPasswordRef.current?.value;
 
     const complete =
       firstnameRef.current?.value &&
       lastnameRef.current?.value &&
-      usernameRef.current?.value &&
+      usernameValid &&
       cityRef.current?.value &&
       emailRef.current?.value &&
       passwordRef.current?.value &&
@@ -43,14 +47,21 @@ export default function Register() {
       passwordMatch;
 
     setIsComplete(!!complete);
+
     if (
       passwordRef.current?.value &&
       confirmPasswordRef.current?.value &&
       !passwordMatch
     ) {
-      setPasswordError("Passwords do not match");
+      setPasswordError("Passwords don't match");
     } else {
       setPasswordError("");
+    }
+
+    if (username.length > 20) {
+      setUsernameError("Username must be 20 characters or less");
+    } else {
+      setUsernameError("");
     }
   };
 
@@ -193,7 +204,10 @@ export default function Register() {
                   onInput={validateForm}
                 />
               </div>
-              {passwordError && <p className="form-error">{passwordError}</p>}
+              <div className="form-error">
+                {passwordError && <p>{passwordError}</p>}
+                {usernameError && <p>{usernameError}</p>}
+              </div>
             </section>
             <div className="register-button-wrapper">
               <button
