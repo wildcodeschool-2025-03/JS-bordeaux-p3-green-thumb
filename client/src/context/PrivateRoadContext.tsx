39d8333e5a77f4238e81b5ef8b3daa9b571751f8
Kevin DeAlmeida <dealmeida.dak@gmail.com>
2025-07-11 16:40:19 +0200

    update privateroad for timeout redirection for better UX, add css file, responsive

--- client/src/context/PrivateRoadContext.tsx
+++ client/src/context/PrivateRoadContext.tsx
@@ -1,6 +1,9 @@
+import { useEffect, useState } from "react";
 import type { ReactNode } from "react";
 import { Navigate } from "react-router";
+import leaf from "../assets/images/Logo-green-thumb.png";
 import { useUserContext } from "../context/UserContext";
+import "./PrivateRoadContext.css";
 
 interface PrivateRoad {
   children: ReactNode;
@@ -8,9 +11,26 @@ interface PrivateRoad {
 
 export default function PrivateRoadContext({ children }: PrivateRoad) {
   const { user } = useUserContext();
+  const [moveToLogin, setMoveToLogin] = useState(false);
+
+  useEffect(() => {
+    if (!user.token) {
+      const timer = setTimeout(() => setMoveToLogin(true), 3000);
+      return () => clearTimeout(timer);
+    }
+  }, [user.token]);
+
+  if (moveToLogin) {
+    return <Navigate to="/login" />;
+  }
 
   if (!user.token) {
-    return <Navigate to="/login" />;
+    return (
+      <section className="alert-box">
+        <img className="leaf-decor" src={leaf} alt="ceci est une feuille" />
+        <p>You will be redirected to the login page</p>
+      </section>
+    );
   }
 
   return <>{children}</>;
