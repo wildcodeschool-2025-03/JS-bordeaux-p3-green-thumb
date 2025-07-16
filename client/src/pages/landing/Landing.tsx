f3b79d12f3eb442881ecc2d43fd6b5710785c2b8
S1mon_G <simonguillo@gmail.com>
2025-07-16 14:29:04 +0200

    modified landing, added right path to font

--- client/src/pages/landing/Landing.tsx
+++ client/src/pages/landing/Landing.tsx
@@ -1,38 +1,45 @@
-import type React from "react";
-
 import "./Landing.css";
 import { useNavigate } from "react-router";
+import phone from "../../../public/images/app-icon/phone-display.png";
 
 import leaf from "../../../public/images/app-icon/leaf.png";
 
-const Landing: React.FC = () => {
+export default function Landing() {
   const navigate = useNavigate();
 
   const Login = () => {
     navigate("/login");
   };
-  const Signup = () => {
+  const Register = () => {
     navigate("/register");
   };
+
   return (
     <div className="landing-container">
-      <div>coucou</div>
-      <div className="logo-section">
-        <div className="logo-icon">
-          <img src={leaf} alt="greenthumb logo" />
+      <section className="landing-banner">
+        <img
+          src={phone}
+          alt="phone representation"
+          className="phone-representation"
+        />
+        <div className="banner-title">
+          <h2>Greenthumb</h2>
+          <hr />
+          <p>your gardening companion</p>
         </div>
+      </section>
 
+      <section className="welcome-section">
+        <img src={leaf} alt="greenthumb logo" className="greenthumb-logo" />
         <h1 className="welcome-title">WELCOME</h1>
-      </div>
 
-      <button type="button" className="btn btn-primary" onClick={Login}>
-        Login
-      </button>
-      <button type="button" className="btn btn-outline" onClick={Signup}>
-        Sign Up
-      </button>
+        <button type="button" className="btn btn-primary" onClick={Login}>
+          Login
+        </button>
+        <button type="button" className="btn btn-outline" onClick={Register}>
+          Register
+        </button>
+      </section>
     </div>
   );
-};
-
-export default Landing;
+}
