a0eb742f686e5648fd8cbaeeefc6b40925b6fb7d
Mickaël Lambert <mickael.lambert@wildcodeschool.com>
2025-07-22 11:59:04 +0200

    Revert "US13_WEATHER"

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,10 @@
 import { Outlet } from "react-router";
 import "./App.css";
-import { Toaster } from "react-hot-toast";
 import Navbar from "./components/navbar/Navbar";
-import { WeatherAlertToaster } from "./components/weather/WeatherAlertToaster";
-import { WeatherCard } from "./components/weather/WeatherCard";
 
 function App() {
   return (
     <>
-      <WeatherCard />
-
-      <WeatherAlertToaster />
-      <Toaster />
       <Outlet />
       <Navbar />
     </>
