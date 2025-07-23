c3f1075e80b973545d0252018d86e8292fc60fe1
S1mon_G <simonguillo@gmail.com>
2025-07-23 15:27:06 +0200

    added callback to usefetchwithauth to prevent infinite loop

--- client/src/tools/useFetchWithAuth.ts
+++ client/src/tools/useFetchWithAuth.ts
@@ -1,25 +1,29 @@
+import { useCallback } from "react";
 import { useUserContext } from "../context/UserContext";
 
 export function useFetchWithAuth() {
   const { user } = useUserContext();
 
-  const fetchWithAuth = async (
-    input: RequestInfo | URL,
-    init: RequestInit = {},
-  ): Promise<Response> => {
-    const headers = new Headers(init.headers || {});
+  const fetchWithAuth = useCallback(
+    async (
+      input: RequestInfo | URL,
+      init: RequestInit = {},
+    ): Promise<Response> => {
+      const headers = new Headers(init.headers || {});
 
-    if (user.token) {
-      headers.set("Authorization", `Bearer ${user.token}`);
-    }
+      if (user.token) {
+        headers.set("Authorization", `Bearer ${user.token}`);
+      }
 
-    const modifiedInit: RequestInit = {
-      ...init,
-      headers,
-    };
+      const modifiedInit: RequestInit = {
+        ...init,
+        headers,
+      };
 
-    return fetch(input, modifiedInit);
-  };
+      return fetch(input, modifiedInit);
+    },
+    [user.token],
+  );
 
   return fetchWithAuth;
 }
